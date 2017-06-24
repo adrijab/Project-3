5799415a19b5a3f99746112dade3310d34c619b9
Adrija Banerjee <adrijabanerjee19496@gmail.com>
2017-06-24 01:11:47 -0700

    web scrapping updated

--- craigslist/craigslist/spiders/craigs_spider.py
+++ craigslist/craigslist/spiders/craigs_spider.py
@@ -1,6 +1,6 @@
 import scrapy
 from craigslist.items import CraigslistItem
-from scrapy.selector import HtmlXPathSelector
+from scrapy.selector import Selector
 from bs4 import BeautifulSoup as bs
 
 
@@ -9,16 +9,27 @@ class CraigsSpider(scrapy.Spider):
     start_urls = ['https://craigslist.org/']
 
     def parse(self,response):
-        hxs = HtmlXPathSelector(response)
-        city = hxs.select("///li[@class='s']//a")
-        url = response.xpath().extract()
-        # titles = response.xpath('//h2/a/@title').extract()
-        # links = response.xpath('//h2/a/@href').extract()
-        # companies = response.xpath('//span[@class="company"]/span').extract()
-        for index, node in enumerate(hxs.select("///li[@class='s']//a")):
+        sel = Selector(response)
+        print sel.xpath("///li[@class='s']//a").extract()
+
+        for index, node in enumerate(sel.xpath("///li[@class='s']//a")):
             x = node.select("@href").extract()[0].encode('utf-8')
-            y = node.select("///li[@class='s']//a/text()").extract()[index]
+            y = node.select("///li[@class='s']//a/text()").extract()[index].encode('utf-8')
             item = CraigslistItem()
             item['city'] = y
             item['url'] = x
             yield item
+
+        # hxs = HtmlXPathSelector(response)
+        # city = hxs.select("///li[@class='s']//a")
+        # url = response.xpath().extract()
+        # # titles = response.xpath('//h2/a/@title').extract()
+        # # links = response.xpath('//h2/a/@href').extract()
+        # # companies = response.xpath('//span[@class="company"]/span').extract()
+        # for index, node in enumerate(hxs.select("///li[@class='s']//a")):
+        #     x = node.select("@href").extract()[0].encode('utf-8')
+        #     y = node.select("///li[@class='s']//a/text()").extract()[index]
+        #     item = CraigslistItem()
+        #     item['city'] = y
+        #     item['url'] = x
+        #     yield item
